76f6bd5a2f6df6d4c1fa9bb1701852a10227148d
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-10-15 00:52:13 +0100

    Update web.py

--- web.py
+++ web.py
@@ -1,1 +1,30 @@
 import os
+import webbrowser
+import random
+from time import sleep
+
+logo = f'''
+          ||            ||
+          ||            ||
+          ||            ||
+          ||      /\    ||
+          ||     /  \   ||
+          ||    /    \  ||
+          ||  /       \ ||
+          ||/          \|| E B R O W S E R
+         
+ |||||||||||||||CODED BY TIMMY|||||||||
+'''
+
+
+def first():
+    search = input("||||||||what do you want to search for>>")
+    url = 'https://www.google.com/{search}
+    print("searching.....")
+    time.sleep(0.2)
+    print("gotten ip address ")
+    webbrowser.open(url)
+
+print(logo)
+first() 
+
